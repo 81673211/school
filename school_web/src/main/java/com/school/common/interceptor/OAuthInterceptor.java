91215f6c62461e0397e0caa42c224822cc22d280
81673211 <81673211@qq.com>
2018-08-07 09:46:33 +0800

    token

--- school_web/src/main/java/com/school/common/interceptor/OAuthInterceptor.java
+++ school_web/src/main/java/com/school/common/interceptor/OAuthInterceptor.java
@@ -12,71 +12,68 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.school.service.customer.CustomerService;
 import com.school.service.wechat.OauthService;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * ClassName: OAuthInterceptor
- * @Description:  接口拦截器 
+ * @Description: 接口拦截器
  * @author linqingsong
  * @date Jan 18, 2016 9:57:39 AM
  */
 @Slf4j
-public class OAuthInterceptor implements HandlerInterceptor{
+public class OAuthInterceptor implements HandlerInterceptor {
 
-	@Autowired
-	private OauthService oauthService;
-	@Autowired
-	private CustomerService customerService;
+    @Autowired
+    private OauthService oauthService;
 
-	@Override
-	public void afterCompletion(HttpServletRequest arg0,
-			HttpServletResponse arg1, Object arg2, Exception arg3) {
+    @Override
+    public void afterCompletion(HttpServletRequest arg0,
+                                HttpServletResponse arg1, Object arg2, Exception arg3) {
 
-	}
+    }
 
-	@Override
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
-			Object arg2, ModelAndView arg3) {
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+                           Object arg2, ModelAndView arg3) {
 
-	}
+    }
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-			Object arg2) {
-		String openId = request.getParameter("openId");
-		if (StringUtils.isBlank(openId)) {
-		    log.info("openId not found");
-		    return false;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object arg2) {
+        String openId = request.getParameter("openId");
+        if (StringUtils.isBlank(openId)) {
+            log.info("openId not found");
+            return false;
         }
-		handleAuthResult(oauthService.check(openId), request, response);
-		return true;
-	}
+        handleAuthResult(oauthService.check(openId), request, response);
+        return true;
+    }
 
-	private void handleAuthResult(int authResult, HttpServletRequest request, HttpServletResponse response) {
-		if (authResult == 1) {
-			Map<String, String[]> parameterMap = request.getParameterMap();
-			String requestURI = request.getRequestURI();
-			StringBuilder state = new StringBuilder();
-			state.append(requestURI);
-			if (!CollectionUtils.isEmpty(parameterMap)) {
-				state.append("?");
-				parameterMap.forEach((key, value) -> {
-					if (!"openId".equals(key)) {
-						state.append(key).append("=").append(value[0]);
-					}
-				});
-			}
-			log.info("re-auth state:{}", state);
-			try {
-				response.sendRedirect(oauthService.getOAuthUrl(state.toString()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			throw new RuntimeException("认证失败");
-		}
-	}
+    private void handleAuthResult(int authResult, HttpServletRequest request, HttpServletResponse response) {
+        if (authResult == 1) {
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            String requestURI = request.getRequestURI();
+            StringBuilder state = new StringBuilder();
+            state.append(requestURI);
+            if (!CollectionUtils.isEmpty(parameterMap)) {
+                state.append("?");
+                parameterMap.forEach((key, value) -> {
+                    if (!"openId".equals(key)) {
+                        state.append(key).append("=").append(value[0]);
+                    }
+                });
+            }
+            log.info("re-auth state:{}", state);
+            try {
+                response.sendRedirect(oauthService.getOAuthUrl(state.toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (authResult == 2) {
+            throw new RuntimeException("认证失败");
+        }
+    }
 }
