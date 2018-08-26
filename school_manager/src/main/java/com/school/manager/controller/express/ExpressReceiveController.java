e6abc6ba7ebcffca6874ccb2fee74945dd8b1c2b
zhouylard <zhouylard@163.com>
2018-08-26 22:55:56 +0800

    收件补全收件人信息

--- school_manager/src/main/java/com/school/manager/controller/express/ExpressReceiveController.java
+++ school_manager/src/main/java/com/school/manager/controller/express/ExpressReceiveController.java
@@ -124,6 +124,13 @@ public class ExpressReceiveController extends BaseEasyWebController {
             if (customer != null) {
                 expressReceive.setCustomerId(customer.getId());
             }
+            // 如果未填写收件人姓名、地址，则自动补全
+            if(StringUtils.isBlank(expressReceive.getReceiverName())){
+            	expressReceive.setReceiverName(customer.getName());
+            }
+            if(StringUtils.isBlank(expressReceive.getReceiverAddr())){
+            	expressReceive.setReceiverAddr(customer.getAddr());
+            }
             expressReceiveService.saveOrUpdate(expressReceive);
             if (customer != null && StringUtils.isNotBlank(customer.getOpenId())) {
                 Integer status = expressReceive.getExpressStatus();
