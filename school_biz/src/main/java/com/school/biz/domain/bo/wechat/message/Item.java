0ae372bfcb784d9974afd5a7d774bf5cdbed836f
zhouylard <zhouylard@163.com>
2018-08-26 11:58:33 +0800

    merge

---
+++ school_biz/src/main/java/com/school/biz/domain/bo/wechat/message/Item.java
@@ -0,0 +1,19 @@
+package com.school.biz.domain.bo.wechat.message;
+
+import lombok.Data;
+
+/**
+ *
+ * <b>Description:.</b><br> 
+ * @author <b>sil.zhou</b>
+ * <br><b>ClassName:</b> 
+ * <br><b>Date:</b> 2018/8/24 10:38
+ */
+@Data
+public class Item {
+
+    private String Title;
+    private String Description;
+    private String PicUrl;
+    private String Url;
+}
