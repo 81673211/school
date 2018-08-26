6a89e8464ec78d7ac9b61152ef72df89258324e3
zhouylard <zhouylard@163.com>
2018-08-26 17:37:21 +0800

    权限更新

--- sql/alph/security/resource_info.sql
+++ sql/alph/security/resource_info.sql
@@ -19,9 +19,15 @@ insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_re
 
 
 /** 2.订单管理 */
-/** 订单列表 */
-insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_remark) values (100, '订单列表', '/order/list.do', 0,(select mi.id from menu_info mi where mi.resource_name = '订单管理'),NULL);
-insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_remark) values (101, '订单详情', '/order/detail.do', (select ri.id from resource_info ri where ri.res_url='/order/list.do'),(select mi.id from menu_info mi where mi.resource_name = '订单管理'),NULL);
+/** 支付订单列表 */
+insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_remark) values (101, '支付订单列表', '/order/list.do', 0,(select mi.id from menu_info mi where mi.resource_name = '支付订单列表'),NULL);
+
+insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_remark) values (102, '支付订单详情', '/order/detail.do', (select ri.id from resource_info ri where ri.res_url='/order/list.do'),(select mi.id from menu_info mi where mi.resource_name = '支付订单列表'),NULL);
+
+/** 退款订单列表 */
+insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_remark) values (151, '退款订单列表', '/refundOrder/list.do', 0,(select mi.id from menu_info mi where mi.resource_name = '退款订单列表'),NULL);
+
+insert into resource_info (id, res_name, res_url, parent_res_id, menu_id, res_remark) values (152, '退款订单详情', '/refundOrder/detail.do', (select ri.id from resource_info ri where ri.res_url='/refundOrder/list.do'),(select mi.id from menu_info mi where mi.resource_name = '退款订单列表'),NULL);
 
 
 /** 3.快递公司管理 */
