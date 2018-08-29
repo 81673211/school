d15fcf36c695e15eab22b84ed2388c74df064060
81673211 <81673211@qq.com>
2018-08-29 21:12:07 +0800

    cache

--- school_biz/src/main/java/com/school/biz/constant/RedisKeyNS.java
+++ school_biz/src/main/java/com/school/biz/constant/RedisKeyNS.java
@@ -18,4 +18,6 @@ public final class RedisKeyNS {
     public static final String CACHE_NAMESPACE_AUTH_TOKEN = "redis:auth:access_token:";
 
     public static final String CACHE_NAMESPACE_SUGGESTION = "redis:suggestion:";
+
+    public static final String CACHE_SEND_EXPRESS_FEE = "redis:send_express_fee";
 }
