61c675a0cd46aed3c0b18abfb3199ae1b2196191
81673211 <81673211@qq.com>
2018-09-03 10:06:00 +0800

    不保存微信昵称

--- school_web/src/main/java/com/school/web/controller/wechat/WechatController.java
+++ school_web/src/main/java/com/school/web/controller/wechat/WechatController.java
@@ -90,7 +90,6 @@ public class WechatController {
         }
         if (StringUtils.isBlank(customer.getAvatar())) {
             UserWechat userWechat = oauthService.getDetail(openId, oAuthToken.getAccessToken());
-            customer.setNickname(userWechat.getNickname());
             customer.setAvatar(userWechat.getAvatar());
             customer.setSex(String.valueOf(userWechat.getSex()));
             customerService.update(customer);
