8ccdc4b74d7265cb9c5290fde14dfc7b4fc2284a
81673211 <81673211@qq.com>
2018-09-05 09:36:06 +0800

    express log

---
+++ school_biz/src/main/java/com/school/biz/domain/entity/log/ExpressLog.java
@@ -0,0 +1,32 @@
+package com.school.biz.domain.entity.log;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ *
+ * <b>Description:.</b><br> 
+ * @author <b>sil.zhou</b>
+ * <br><b>ClassName:</b> 
+ * <br><b>Date:</b> 2018/9/4 21:58
+ */
+@Data
+public class ExpressLog {
+
+    private Long id;
+
+    private Long expressId;
+
+    private int expressType;
+
+    private String action;
+
+    private String preStatus;
+
+    private String postStatus;
+
+    private String operator;
+
+    private Date operateTime;
+}
