4d14a1b1591dee6390f49f749572949f930e4b60
zhouylard <zhouylard@163.com>
2018-09-13 22:23:37 +0800

    isRefundFeeIllegal

--- school_biz/src/main/java/com/school/biz/service/order/impl/OrderInfoServiceImpl.java
+++ school_biz/src/main/java/com/school/biz/service/order/impl/OrderInfoServiceImpl.java
@@ -318,16 +318,18 @@ public class OrderInfoServiceImpl extends BaseServiceImpl<OrderInfo, OrderInfoMa
     private boolean isRefundFeeIllegal(ExpressSend expressSend, List<OrderInfo> orderInfos, BigDecimal refundFee) {
         boolean flag = true;
 
-        // 取出订单服务费
+        // 门槛费：服务费+服务费补单
         BigDecimal serviceAmt = expressSend.getServiceAmt();
+        BigDecimal reOrderServiceAmt = expressSend.getReOrderServiceAmt();
+        BigDecimal serviceFee = serviceAmt.add(reOrderServiceAmt);
 
-        // 最大可退金额=每笔可退金额之和-服务费
+        // 最大可退金额=每笔可退金额之和-（服务费+服务费补单）
         BigDecimal refundCurrMaxAmt = new BigDecimal("0");
         // 取出每笔成功订单（订单金额-已退款金额），即每笔订单可退金额
         for (OrderInfo orderInfo : orderInfos) {
             refundCurrMaxAmt = refundCurrMaxAmt.add(orderInfo.getAmount().subtract(orderInfo.getRefundAmt()));
         }
-        BigDecimal finalRefundAmt = refundCurrMaxAmt.subtract(serviceAmt);
+        BigDecimal finalRefundAmt = refundCurrMaxAmt.subtract(serviceFee);
 
         if (refundFee.compareTo(finalRefundAmt) == 1) {
             flag = false;
