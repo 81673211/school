310085dde41c3de89157c919964d82ea97e9df20
81673211 <81673211@qq.com>
2018-09-20 14:04:50 +0800

    活动短信删除

--- school_web/src/main/java/com/school/web/controller/BackDoorController.java
+++ school_web/src/main/java/com/school/web/controller/BackDoorController.java
@@ -1,16 +1,9 @@
 package com.school.web.controller;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.school.biz.service.customer.CustomerService;
-import com.school.biz.service.message.SmsService;
 import com.school.web.vo.response.Response;
 
 /**
@@ -24,21 +17,10 @@ import com.school.web.vo.response.Response;
 @RequestMapping("/backDoor")
 public class BackDoorController {
 
-    @Autowired
-    private CustomerService customerService;
-    @Autowired
-    private SmsService smsService;
-
     @RequestMapping("/918")
     @ResponseBody
-    public Response send918() throws InterruptedException {
-        List<String> phones = customerService.selectRegisteredPhone();
-        if (!CollectionUtils.isEmpty(phones)) {
-            for (String phone : phones) {
-                smsService.sendActivaty918(phone);
-                TimeUnit.MILLISECONDS.sleep(500);
-            }
-        }
+    public Response send918() {
+
         return new Response().writeSuccess("success");
     }
 }
